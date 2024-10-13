f5e1fb2ac98ef362e8a4cb48f9bf2a602b926dc5
SeYeonHa <hase9708@naver.com>
2024-10-14 00:09:48 +0900

    feat: pay approve ver.PC

---
+++ src/main/java/shop/mtcoding/todayhome/core/util/PaymentSession.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.todayhome.core.util;
+
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.Objects;
+
+public class PaymentSession {
+
+    public static void addAttribute(String name, Object value) {
+        Objects.requireNonNull(RequestContextHolder.getRequestAttributes()).setAttribute(name, value, RequestAttributes.SCOPE_SESSION);
+    }
+
+    public static String getStringAttributeValue(String name) {
+        return String.valueOf(getAttribute(name));
+    }
+
+    public static Object getAttribute(String name) {
+        return Objects.requireNonNull(RequestContextHolder.getRequestAttributes()).getAttribute(name, RequestAttributes.SCOPE_SESSION);
+    }
+}
---
+++ src/main/java/shop/mtcoding/todayhome/payment/PaymentService.java
@@ -0,0 +1,66 @@
+package shop.mtcoding.todayhome.payment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import shop.mtcoding.todayhome.core.error.ex.ExceptionApi404;
+import shop.mtcoding.todayhome.core.util.PaymentSession;
+import shop.mtcoding.todayhome.ready.ReadyResponse;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    // 결제 승인 요청
+    @Transactional
+    public void payApprove(String pgToken) {
+
+
+        String tid = PaymentSession.getStringAttributeValue("tid");
+        Payment payment = paymentRepository.mFindByTid(tid)
+                .orElseThrow(() -> new ExceptionApi404("세션을 찾을 수 없습니다"));
+
+        String partnerOrderId = payment.getOrder().getPartnerOrderId();
+        String partnerUserId = payment.getOrder().getUser().getName();
+
+    // 시크릿 키
+        String secretKeyDev = "DEVE0E51377133628B9BF75F0644B4A09DAF4552";
+
+    // 1. 결제 필요한 header 데이터
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "DEV_SECRET_KEY " + secretKeyDev);
+        headers.set("Content-Type", "application/json");
+
+        // 2. 전송할 바디 설정
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("cid", "TC0ONETIME");
+        parameters.put("tid", tid);
+        parameters.put("partner_order_id", partnerOrderId);
+        parameters.put("partner_user_id", partnerUserId);
+        parameters.put("pg_token", pgToken);
+
+        // 3. 데이터 전송
+        HttpEntity<Map<String, String>> entityMap = new HttpEntity<>(parameters, headers);
+
+        RestTemplate template = new RestTemplate();
+        String url = "https://open-api.kakaopay.com/online/v1/payment/approve";
+
+        ResponseEntity<PaymentResponse.DTO> response = template.postForEntity(url, entityMap, PaymentResponse.DTO.class);
+        payment.setPgToken(pgToken);
+        paymentRepository.save(payment);
+
+    }
+
+}
