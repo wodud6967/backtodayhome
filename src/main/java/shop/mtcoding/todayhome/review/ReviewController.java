12effdc6ebbfe88412157b59f95b81de2a836a5c
eunmouse <ejha1208@gmail.com>
2024-10-13 18:54:47 +0900

    feat: 리뷰수정

--- src/main/java/shop/mtcoding/todayhome/review/ReviewController.java
+++ src/main/java/shop/mtcoding/todayhome/review/ReviewController.java
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import shop.mtcoding.todayhome.core.util.Resp;
 import shop.mtcoding.todayhome.user.User;
@@ -19,9 +21,14 @@ public class ReviewController {
     @PostMapping("/api/review")
     public ResponseEntity<?> save(@RequestBody ReviewRequest.SaveDTO saveDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-//        System.out.println(sessionUser);
-//        System.out.println(sessionUser.getId());
         ReviewResponse.DTO reviewDTO = reviewService.리뷰쓰기(saveDTO, sessionUser);
+        return ResponseEntity.ok(Resp.ok(reviewDTO));
+    }
+
+    @PutMapping("/api/review/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody ReviewRequest.UpdateDTO updateDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        ReviewResponse.DTO reviewDTO = reviewService.리뷰수정(id, updateDTO, sessionUser);
         return ResponseEntity.ok(Resp.ok(reviewDTO));
     }
 
